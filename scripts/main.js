c22d8795c9bb82cfdbde1940cfe615ab49e5ad62
shiblinoru <web.is.only@gmail.com>
2022-06-29 18:57:54 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var p = "Кимрский район образован 12 июля 1929 года в составе Кимрского округа Московской области.";
+document.getElementById("three_a").innerHTML = p;
+
+var p = "Привет из колхоза! Мы представляем Вашему вниманию новый проект. Это проект сайта" + 
+" деревни Шиблино.";
+document.getElementById("four_a").innerHTML = p;
+
+/*function logo() {
+var img =newImage();
+img.src='../image/logo.img';
+document.querySelector('.container') .appendChild(img);
+document.getElementById('three_a').innerHTML = img;}*/
